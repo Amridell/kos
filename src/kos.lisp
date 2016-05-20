1f4c17f947ad30689ed36e38c540ec646079f2df
Grant Vesely <fouric@users.noreply.github.com>
2016-05-19 21:17:57 -0700

    Added MAKE-MAP, which just actually makes a single, boring room

--- src/kos.lisp
+++ src/kos.lisp
@@ -1,4 +1,27 @@
 (in-package :kos)
 
+(defmacro doitimes (count &body body)
+  `(dotimes (,(gensym) ,count)
+	    ,@body))
+
+(defun make-map (x y)
+  (check-type x (integer 2 *))
+  (check-type x (integer 2 *))
+  (let (map)
+    (let (bottom-wall)
+      (doitimes y
+	(push :wall bottom-wall))
+      (push bottom-wall map))
+    (doitimes (- x 2)
+      (let (mid-wall)
+	(push :wall mid-wall)
+	(doitimes (- y 2)
+	  (push :floor mid-wall))
+	(push mid-wall map)))
+    (let (top-wall)
+      (doitimes y
+	(push :wall top-wall))
+      (push top-wall map))))
+
 (defun kos ()
   (princ "Welcome to King of Shadows"))
