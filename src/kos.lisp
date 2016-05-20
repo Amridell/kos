658ba59e3512723f5d3c774e79873bcf5e4b7e05
Grant Vesely <fouric@users.noreply.github.com>
2016-05-20 15:03:53 -0700

    Moved map-floor code into a message-passing object

--- src/kos.lisp
+++ src/kos.lisp
@@ -4,46 +4,41 @@
   `(dotimes (,(gensym) ,count)
 	    ,@body))
 
-(defstruct map-floor
-  (tiles nil :type list)
-  (width nil :type integer)
-  (height nil :type integer))
-
-(defun make-map-floor-tiles (x y)
-  (check-type x (integer 2 *))
-  (check-type x (integer 2 *))
-  (let (map)
-    ;; make the bottom wall
-    (let (bottom-wall)
-      (doitimes y
-	(push :wall bottom-wall))
-      (push bottom-wall map))
-    ;; make all of the "sandwich" rows
-    (doitimes (- x 2)
-      (let (mid-wall)
-	(push :wall mid-wall)
-	(doitimes (- y 2)
-	  (push :floor mid-wall))
-	(push :wall mid-wall)
-	(push mid-wall map)))
-    ;; make the top wall
-    (let (top-wall)
-      (doitimes y
-	(push :wall top-wall))
-      (push top-wall map))))
-
-(defun print-map-floor (map width height)
-  (declare (ignore height))
-  (dolist (row map)
-    (dotimes (x width)
-      (princ (case (nth x row)
-	       (:wall #\#)
-	       (:floor #\.))))
-    (princ #\Newline)))
+(defun make-map-floor (width height)
+  (check-type width (integer 2 *))
+  (check-type height (integer 2 *))
+  (let ((tiles (let (map)
+		 ;; make the bottom wall
+		 (let (bottom-wall)
+		   (doitimes height
+		     (push :wall bottom-wall))
+		   (push bottom-wall map))
+		 ;; make all of the "sandwich" rows
+		 (doitimes (- width 2)
+		   (let (mid-wall)
+		     (push :wall mid-wall)
+		     (doitimes (- height 2)
+		       (push :floor mid-wall))
+		     (push :wall mid-wall)
+		     (push mid-wall map)))
+		 ;; make the top wall
+		 (let (top-wall)
+		   (doitimes height
+		     (push :wall top-wall))
+		   (push top-wall map))
+		 map)))
+    (lambda (name &rest args)
+      (declare (ignore args))
+      (case name
+	(:print
+	 (dolist (row tiles)
+	   (dotimes (x width)
+	     (princ (case (nth x row)
+		      (:wall #\#)
+		      (:floor #\.))))
+	   (princ #\Newline)))))))
 
 (defun kos ()
   (format t "Welcome to King of Shadows!~%")
-  (let ((map (make-map-floor :tiles (make-map-floor-tiles 5 5)
-			     :width 5
-			     :height 5)))
-    (print-map-floor (map-floor-tiles map) 5 5)))
+  (let ((map-floor (make-map-floor 5 5)))
+    (funcall map-floor :print)))
