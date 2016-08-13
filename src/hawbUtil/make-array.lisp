d2ceb6440a074402dc7ec472b0242e3c411c8ba2
test <you@example.com>
2016-08-13 03:19:56 -0700

    added some functionality to the convert-lisp file

--- src/hawbUtil/make-array.lisp
+++ src/hawbUtil/make-array.lisp
@@ -1,18 +1,19 @@
+
 ;;checks the inputted map to see if all of the 
 ;;inner lists are equi-length, if so, makes a 2d array
 (defun make-map-array (map) 
   (if (apply #'= (mapcar #'length map))
       (make-array (list (length map) (length (first map)))
-		  :initial-contents map)
+      :initial-contents map)
       nil))
 
 
 (defun make-array-test ()
-  (let ((map) (thing))
+  (let (map)
     (setq map (list (list #\#  #\#  #\#  #\#  #\#  #\#) 
-		    (list #\#  #\.  #\@  #\.  #\.  #\#) 
-		    (list #\#  #\.  #\.  #\@  #\@  #\#)  
-		    (list #\#  #\@  #\.  #\.  #\.  #\#) 
-		    (list #\#  #\#  #\#  #\#  #\#  #\#)))
+        (list #\#  #\.  #\@  #\.  #\.  #\#) 
+        (list #\#  #\.  #\.  #\@  #\@  #\#)  
+        (list #\#  #\@  #\.  #\.  #\.  #\#) 
+        (list #\#  #\#  #\#  #\#  #\#  #\#)))
 
-    (print (make-map-array map))))
+    (format t "~a~%" (make-map-array map))))
