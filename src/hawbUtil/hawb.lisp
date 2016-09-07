aaf31de2e090e0d2374eaf8e5417e9dcf71fcde7
test <you@example.com>
2016-09-06 21:02:20 -0700

    changed some of the functions of hawb.lisp, it now has some functionality

--- src/hawbUtil/hawb.lisp
+++ src/hawbUtil/hawb.lisp
@@ -1,5 +1,7 @@
 ;;;this is the file that Hawbs will write for things
 
+(ql:quickload :tcod)
+
 ;;converts a file into a list of lines ("strings")
 (defun get-file (filename)
     (with-open-file (stream filename)
@@ -24,13 +26,69 @@
 ;;;prints out a 2d array for the thing that is room.txt
 ;;;(print (make-map-array (convert-room "room.txt")))
 
-(defun print-room (array)
-	if ()
-	()
-	(format t "error in reading the '@'"))
+(defun print-room (given-array)
+	;;if ()
+	;;()
+	;;(format t "error in reading the '@'"))
+
+	(destructuring-bind (height width) (array-dimensions given-array)
+
+		(loop for y from 0 to (- height 1)
+			do (loop for x from 0 to (- width 1)
+				;do (write-char (aref given-array y x))
+				do (tcod::console-put-char tcod:*root* x y (char-code (aref given-array y x)) :set)))))
+				;do (tcod:console-print tcod:*root* x y (write-to-string (aref given-array y x))))
+			;do (format t "~%"))))
+
+				;;do (tcod:console-print tcod:*root* ))))
 
 ;;;fix this thing
 ;;;(position #\t (make-array '(2 2) :initial-contents '((#\a #\h) (#\g #\t))))
 
 
 ;;;(tcod:console-print tcod:*root* 1 1 "thing")
+
+
+(tcod:console-set-custom-font
+   (merge-pathnames #p"data/fonts/arial10x10.png"
+                    (asdf/system:system-source-directory
+                     (asdf:find-system "atemzug-rl")))
+   '(:FONT-TYPE-GREYSCALE :FONT-LAYOUT-TCOD))
+
+(defparameter *x* 45)
+(defparameter *y* 20)
+(defparameter *running* t)
+
+(defun main-run ()
+	(tcod:console-init-root 80 50 :title "Libtcod Lisp Tutorial" :fullscreen? nil :renderer :renderer-sdl)
+	(loop
+	   while *running*
+	   do 
+		 (tcod::console-clear tcod:*root*)
+
+		 (print-room (make-map-array (convert-room "room.txt")))
+
+		 (tcod::console-put-char tcod:*root* *x* *y* (char-code #\@) :set)		  
+		 (tcod::console-flush)
+		
+		 (let ((events (tcod:sys-get-events)))
+		 	(if (tcod:is-key-pressed? :UP) (decf *y*))
+				  	
+		   (loop
+			  for e in events
+			  do 
+				(format t "~A~%" e)
+				(case (car e)
+					(:event-key-release (incf *x*))
+				  	(:event-key (incf *x*))
+				  	(:event-mouse-press (setf *running* nil)))))
+		 (if (not (tcod:legal-console-coordinates? tcod:*root* *x* *y*)) 
+		 	(if (< *y* 0) (setf *y* 10))
+		 	(if (> *y* 79) (setf *y* 49)))))
+
+
+(defun rerun ()
+  (setf *running* t)
+  (main-run))
+
+(main-run)
